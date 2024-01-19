39e0c5a6e856cb765eaf690cf82021711becfd87
anshumancodes <anshumanprof01@gmail.com>
2024-01-19 23:08:42 +0530

    added script

--- htdocs/index.js
+++ htdocs/index.js
@@ -1,2 +1,20 @@
-import 'htmx.org';
-window.htmx = require('htmx.org');
+
+
+// for faq section
+document.addEventListener('DOMContentLoaded', function () {
+    const faqItems = document.querySelectorAll('.faq');
+
+    faqItems.forEach(item => {
+        item.addEventListener('click', () => {
+            const answer = item.nextElementSibling;
+
+            if (answer.classList.contains('hidden')) {
+                answer.classList.remove('hidden');
+            } else {
+                answer.classList.add('hidden');
+            }
+        });
+    });
+});
+
+console.log("js is working");
